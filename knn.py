0990c7e0a85940fa30773db9c0db0cee2398fb34
0877516165za <cherzabang@gmail.com>
2025-04-09 12:03:28 +0700

    Update knn.py

--- knn.py
+++ knn.py
@@ -20,3 +20,8 @@ with col2:
 with col3:
    st.header("Setosa")
    st.image("./img/iris3.jpg")
+
+   html_7 = """
+<div style="background-color:#c5f18a;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>ข้อมูล iris หรือข้อมูลดอกไม้สำหรับทำนาย</h5></center>
+</div>
