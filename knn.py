735008f3fa873367368c7821d9e3627c64b3e196
0877516165za <cherzabang@gmail.com>
2025-04-09 12:00:55 +0700

    Update knn.py

--- knn.py
+++ knn.py
@@ -6,3 +6,17 @@ import matplotlib.pyplot as plt
 
 st.title('การจําแนกข้อมูลด้วยเทคนิค Machine Learning')
 st.image("./img/red.jpg")
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+   st.header("Versicolor")
+   st.image("./img/iris1.jpg")
+
+with col2:
+   st.header("Verginiga")
+   st.image("./img/iris2.jpg")
+
+with col3:
+   st.header("Setosa")
+   st.image("./img/iris3.jpg")
